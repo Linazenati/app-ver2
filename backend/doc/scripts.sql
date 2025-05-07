69c04fbeace6effdd4172749702840ea2936fbc5
Lina <zenatilina2002@gmail.com>
2025-05-07 20:30:27 +0100

    Ajout du code source + .gitignore pour ignorer node_modules

---
+++ backend/doc/scripts.sql
@@ -0,0 +1,5 @@
+-- password = 123
+INSERT INTO `Utilisateurs` 
+    (`id`, `nom`, `prenom`, `email`, `password`, `telephone`, `role`, `createdAt`, `updatedAt`) 
+VALUES (NULL, 'MENSER', 'Fayçal', 'menser.faycal@gmail.com', '$2b$10$/yHK/aQNGNHNaIF0Y27apuRNMe8bhdscAqkqQBbJ276JG8UEiMqwq', '0777777754', 'administrateur', '2025-04-16 11:40:36.000000', '2025-04-16 11:40:36.000000');
+
